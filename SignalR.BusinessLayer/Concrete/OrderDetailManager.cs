e9fc7b9cce66901263842c06b8ab05791b022783
TKaann <tkaanisn@gmail.com>
2025-01-30 15:47:57 +0300

    Order & OrderDetail
    Order & OrderDetail Registiration ve mimari kisimlarinin yapilmasi.

---
+++ SignalR.BusinessLayer/Concrete/OrderDetailManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class OrderDetailManager : IOrderDetailService
+    {
+        private readonly IOrderDetailDal _orderDetailService;
+
+        public OrderDetailManager(IOrderDetailDal orderDetailService)
+        {
+            _orderDetailService = orderDetailService;
+        }
+
+        public void TAdd(OrderDetail entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(OrderDetail entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderDetail TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderDetail> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(OrderDetail entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SignalR.BusinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    private readonly IOrderDal _orderDal;
+    public class OrderManager : IOrderService
+    {
+        public void TAdd(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
