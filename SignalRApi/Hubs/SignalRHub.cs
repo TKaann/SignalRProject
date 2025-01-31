6ca62010e747e4d0903df05ceec91a50661b3720
TKaann <tkaanisn@gmail.com>
2025-01-31 16:18:12 +0300

    Refactor
    Refactor SignalR Statistic

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -15,22 +15,18 @@ namespace SignalRApi.Hubs
             _productService = productService;
         }
 
-        public async Task SendCategoryCount()
+        public async Task SendStatistic()
         {
             var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        }
-        public async Task SendProductCount()
-        {
+
             var value2 = _productService.TProductCount();
             await Clients.All.SendAsync("ReceiveProductCount", value2);
-        }
-        
-        public async Task ActivePassiveCategoryCount()
-        {
+
             var value3 = _categoryService.TActiveCategoryCount();
-            var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
         }
     }
