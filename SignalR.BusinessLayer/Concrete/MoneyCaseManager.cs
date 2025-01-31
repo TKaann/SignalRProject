3336b419b16761018a57e452888c839273b024bb
TKaann <tkaanisn@gmail.com>
2025-01-31 12:11:48 +0300

    MoneyCases
    The service from which the value in the moneycases table was fetched was written. That is, the service from which the total amount won was brought was written.

---
+++ SignalR.BusinessLayer/Concrete/MoneyCaseManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class MoneyCaseManager : IMoneyCaseService
+    {
+        private readonly IMoneyCaseDal _moneyucaseDal;
+
+        public MoneyCaseManager(IMoneyCaseDal moneyucaseDal)
+        {
+            _moneyucaseDal = moneyucaseDal;
+        }
+
+        public void TAdd(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MoneyCase TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MoneyCase> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TTotalMoneyCaseAmount()
+        {
+            return _moneyucaseDal.TotalMoneyCaseAmount();
+        }
+
+        public void TUpdate(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SignalR.DataAccesLayer/EntityFramework/EfMoneyCaseDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.DataAccesLayer.Concrete;
+using SignalR.DataAccesLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccesLayer.EntityFramework
+{
+    public class EfMoneyCaseDal : GenericRepository<MoneyCase>, IMoneyCaseDal
+    {
+        public EfMoneyCaseDal(SignalRContext context) : base(context)
+        {
+
+        }
+
+        public decimal TotalMoneyCaseAmount()
+        {
+            using var context = new SignalRContext();
+            return context.MoneyCases.Select(x=>x.TotalAmount).FirstOrDefault();
+        }
+    }
+}
