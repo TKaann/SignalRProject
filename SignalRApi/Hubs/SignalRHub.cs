2cf66c69acc96989eeff21e1f55a56994b2f8c32
TKaann <tkaanisn@gmail.com>
2025-01-31 17:23:28 +0300

    Order statistics SignalR
    Order Statistics SignalR Add

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -8,11 +8,13 @@ namespace SignalRApi.Hubs
     {
         private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
 
-        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService)
         {
             _categoryService = categoryService;
             _productService = productService;
+            _orderService = orderService;
         }
 
         public async Task SendStatistic()
@@ -40,6 +42,18 @@ namespace SignalRApi.Hubs
 
             var value8 = _productService.TProductNameByMaxPrice();
             await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
+
+            var value9 = _productService.TProductNameByMinPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value9);
+
+            var value10 = _productService.TProductAvgPriceByHamburger();
+            await Clients.All.SendAsync("ReceiveProductAvgPriceByHamburger", value10.ToString("0.00") + "₺");
+
+            var value11 = _orderService.TTotalOrderCount();
+            await Clients.All.SendAsync("ReceiveTotalOrderCount", value11); 
+            
+            var value12 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value12);
         }
     }
 }
