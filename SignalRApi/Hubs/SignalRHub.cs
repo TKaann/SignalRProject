8f937927450d34a977a3c96e0f8a86f6456ff0ac
TKaann <tkaanisn@gmail.com>
2025-01-31 16:15:13 +0300

    Statistics SignalR
    Writing cartboards where statistical values ​​are reflected instantly with signalR

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,37 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccesLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
     public class SignalRHub:Hub
     {
-        SignalRContext context = new SignalRContext();
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
 
         public async Task SendCategoryCount()
         {
-            var value = context.Categories.Count();
+            var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
+        public async Task SendProductCount()
+        {
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+        }
+        
+        public async Task ActivePassiveCategoryCount()
+        {
+            var value3 = _categoryService.TActiveCategoryCount();
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
         }
     }
 }
