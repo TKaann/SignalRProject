94bd19dad14962dd2a0100af9799038af0ed4ddc
TKaann <tkaanisn@gmail.com>
2025-02-01 12:51:45 +0300

    Layout
    Layout Revised

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutScrpitComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScrpitComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
