11f94094907687cc37a6801b6b211da4897dca80
TKaann <tkaanisn@gmail.com>
2025-02-03 13:19:32 +0300

    Add Basket
    Add Basket Class

---
+++ SignalR.DataAccesLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.DataAccesLayer.Concrete;
+using SignalR.DataAccesLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccesLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalRContext context) : base(context)
+        {
+
+        }
+
+        public List<Basket> GetBasketByMenuTableNumber(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Where(x => x.MenuTableID == id).ToList();
+            return values;
+        }
+    }
+}
