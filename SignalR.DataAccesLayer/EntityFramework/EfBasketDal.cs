0329da8773056b37dd48ff8a7b3e45836bf993c3
TKaann <tkaanisn@gmail.com>
2025-02-03 14:44:30 +0300

    Include
    Include Method Added for productname

--- SignalR.DataAccesLayer/EntityFramework/EfBasketDal.cs
+++ SignalR.DataAccesLayer/EntityFramework/EfBasketDal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SignalR.DataAccesLayer.Abstract;
 using SignalR.DataAccesLayer.Concrete;
 using SignalR.DataAccesLayer.Repositories;
@@ -20,7 +21,7 @@ namespace SignalR.DataAccesLayer.EntityFramework
         public List<Basket> GetBasketByMenuTableNumber(int id)
         {
             using var context = new SignalRContext();
-            var values = context.Baskets.Where(x => x.MenuTableID == id).ToList();
+            var values = context.Baskets.Where(x => x.MenuTableID == id).Include(y=>y.Product).ToList();
             return values;
         }
     }
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text.Json.Serialization;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccesLayer.Abstract;
@@ -67,6 +68,9 @@ builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 
 builder.Services.AddScoped<IBasketService, BasketManager>();
 builder.Services.AddScoped<IBasketDal, EfBasketDal>();
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
