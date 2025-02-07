9306f28a59af5ce13b52fcf89410ff1fe195573c
TKaann <tkaanisn@gmail.com>
2025-02-07 22:28:19 +0300

    Message Revised
    Message Revised

---
+++ SignalR.DataAccesLayer/EntityFramework/EfMessageDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccesLayer.Abstract;
+using SignalR.DataAccesLayer.Concrete;
+using SignalR.DataAccesLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccesLayer.EntityFramework
+{
+    public class EfMessageDal : GenericRepository<Message>, IMessageDal
+    {
+        public EfMessageDal(SignalRContext context) : base(context)
+        {
+
+        }
+    }
+}
