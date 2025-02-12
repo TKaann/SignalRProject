c64793c5b0c889a8d66760759d56e56464cae13a
TKaann <tkaanisn@gmail.com>
2025-02-12 18:25:05 +0300

    revised basket
    revised basket final dynamic table id

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -14,32 +14,29 @@ namespace SignalRApi.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketService _basketService;
-
         public BasketController(IBasketService basketService)
         {
             _basketService = basketService;
         }
-
         [HttpGet]
-        public IActionResult GetBasketByMenuTableID(int id) 
+        public IActionResult GetBasketByMenuTableID(int id)
         {
             var values = _basketService.TGetBasketByMenuTableNumber(id);
             return Ok(values);
         }
-
         [HttpGet("BasketListByMenuTableWithProductName")]
         public IActionResult BasketListByMenuTableWithProductName(int id)
         {
             using var context = new SignalRContext();
             var values = context.Baskets.Include(x => x.Product).Where(y => y.MenuTableID == id).Select(z => new ResultBasketListWithProducts
             {
-                BasketID=z.BasketID,
-                Count=z.Count,
-                MenuTableID=z.MenuTableID,
-                Price=z.Price,
-                ProductID=z.ProductID,
-                TotalPrice=z.TotalPrice,
-                ProductName=z.Product.ProductName
+                BasketID = z.BasketID,
+                Count = z.Count,
+                MenuTableID = z.MenuTableID,
+                Price = z.Price,
+                ProductID = z.ProductID,
+                TotalPrice = z.TotalPrice,
+                ProductName = z.Product.ProductName
             }).ToList();
             return Ok(values);
         }
@@ -51,19 +48,19 @@ namespace SignalRApi.Controllers
             _basketService.TAdd(new Basket()
             {
                 ProductID = createBasketDto.ProductID,
+                MenuTableID = createBasketDto.MenuTableID,
                 Count = 1,
                 Price = context.Products.Where(x => x.ProductID == createBasketDto.ProductID).Select(y => y.Price).FirstOrDefault(),
-                TotalPrice = 0
+                TotalPrice = createBasketDto.TotalPrice,
             });
             return Ok();
         }
-
         [HttpDelete("{id}")]
-        public IActionResult DeleteBasket(int id) 
+        public IActionResult DeleteBasket(int id)
         {
             var value = _basketService.TGetByID(id);
             _basketService.TDelete(value);
-            return Ok("Sepetteki Secilen Urun Silindi.");
+            return Ok("Sepetteki Seçilen Ürün Silindi");
         }
     }
 }
